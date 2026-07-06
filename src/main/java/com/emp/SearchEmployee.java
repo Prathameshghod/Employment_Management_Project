6e5ab38815cc2a09ee607adba6ba1208c920a2c5
prathameshghod <prathameshghodmare10@gmail.com>
2026-07-07 00:09:51 +0530

    delete done

--- src/main/java/com/emp/SearchEmployee.java
+++ src/main/java/com/emp/SearchEmployee.java
@@ -10,6 +10,10 @@ public class SearchEmployee {
                 break;
             }
         }
-
+        System.out.println("Empid" + Main.EmpTable[idx].Empid);
+        System.out.println("Name" + Main.EmpTable[idx].name);
+        System.out.println("Designation" + Main.EmpTable[idx].designation);
+        System.out.println("age" + Main.EmpTable[idx].age);
+        System.out.println("Experience" + Main.EmpTable[idx].exp);
     }
 }
